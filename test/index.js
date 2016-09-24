843eca9f6b1fa4e83dad94a4d5de6d6d36483530
Bartosz Czerwonka <czerwonka.bartosz@gmail.com>
2016-09-24 03:37:44 +0200

    Changed sync method to get service optiosn to async

--- test/index.js
+++ test/index.js
@@ -7,15 +7,13 @@ const expect = chai.expect
 
 const ETCDRegistry = require('../lib/index')
 
+const ETCD_SERVER = 'http://etcd:2379'
+
 describe('ETCD Registry', () => {
   afterEach((done) => {
-    const etcd = new Etcd('http://etcd:2379')
+    const etcd = new Etcd(ETCD_SERVER)
 
     etcd.get('services', (error, services) => {
-      if (error) {
-        throw error
-      }
-
       if (services) {
         etcd.del('services', { recursive: true }, () => {
           etcd.del('services-options', { recursive: true }, done)
@@ -28,13 +26,13 @@ describe('ETCD Registry', () => {
 
   describe('constructor()', () => {
     it('should synchronize local list of services after init', () => {
-      const etcd = new ETCDRegistry('http://etcd:2379')
+      const etcd = new ETCDRegistry(ETCD_SERVER)
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
       etcd.register('test1', { address: 'test' }, { a: 1, b: 2 })
       etcd.register('test2', { address: 'test' }, { a: 1, b: 2 })
 
-      const etcd2 = new ETCDRegistry('http://etcd:2379')
+      const etcd2 = new ETCDRegistry(ETCD_SERVER)
 
       expect(etcd2.getService('test').length).to.be.equal(1)
       expect(etcd2.getService('test1').length).to.be.equal(1)
@@ -44,7 +42,7 @@ describe('ETCD Registry', () => {
 
   describe('register()', () => {
     it('should register service', () => {
-      const etcd = new ETCDRegistry('http://etcd:2379')
+      const etcd = new ETCDRegistry(ETCD_SERVER)
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
@@ -59,8 +57,8 @@ describe('ETCD Registry', () => {
     })
 
     it('should register service and synchronize between all clients', () => {
-      const etcd = new ETCDRegistry('http://etcd:2379')
-      const etcd2 = new ETCDRegistry('http://etcd:2379')
+      const etcd = new ETCDRegistry(ETCD_SERVER)
+      const etcd2 = new ETCDRegistry(ETCD_SERVER)
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
@@ -75,7 +73,7 @@ describe('ETCD Registry', () => {
     })
 
     it('should register service with TTL', (done) => {
-      const etcd = new ETCDRegistry('http://etcd:2379', { ttl: 5 })
+      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 5 })
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
@@ -89,29 +87,37 @@ describe('ETCD Registry', () => {
   })
 
   describe('getServiceOptions()', () => {
-    it('should return options of service', () => {
-      const etcd = new ETCDRegistry('http://etcd:2379')
+    it('should return options of service', (done) => {
+      const etcd = new ETCDRegistry(ETCD_SERVER)
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
-      const options = etcd.getServiceOptions('test')
+      etcd.getServiceOptions('test')
+        .then((options) => {
+          expect(options.a).to.be.equal(1)
+          expect(options.b).to.be.equal(2)
 
-      expect(options.a).to.be.equal(1)
-      expect(options.b).to.be.equal(2)
-    })
+          done()
+        })
+    }).timeout(30 * 1000)
 
-    it('should return undefined if servicem does not exist', () => {
-      const etcd = new ETCDRegistry('http://etcd:2379')
+    it('should return undefined if servicem does not exist', (done) => {
+      const etcd = new ETCDRegistry(ETCD_SERVER)
 
-      const options = etcd.getServiceOptions('test')
+      etcd.register('test123', { address: 'test' }, { a: 1, b: 2 })
 
-      expect(options).to.be.undefined
-    })
+      etcd.getServiceOptions('test')
+        .then((options) => {
+          expect(options).to.be.undefined
+
+          done()
+        })
+    }).timeout(30 * 1000)
   })
 
   describe('renew()', () => {
     it('should renew TTL', (done) => {
-      const etcd = new ETCDRegistry('http://etcd:2379', { ttl: 5 })
+      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 5 })
 
       const id = etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
@@ -128,13 +134,13 @@ describe('ETCD Registry', () => {
     }).timeout(30 * 1000)
 
     it('should do nothing if nodes do not exist', () => {
-      const etcd = new ETCDRegistry('http://etcd:2379', { ttl: 5 })
+      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 5 })
 
       etcd.renew('test', 9999999)
     })
 
     it('should do nothing if node does not exist', () => {
-      const etcd = new ETCDRegistry('http://etcd:2379', { ttl: 5 })
+      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 5 })
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
       etcd.renew('test', 9999999)
