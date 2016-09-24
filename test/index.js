3900307156a46b508df9ce0b70c296e5185dd4ad
Bartosz Czerwonka <czerwonka.bartosz@gmail.com>
2016-09-24 14:32:30 +0200

    Changed sync method to async

--- test/index.js
+++ test/index.js
@@ -14,7 +14,7 @@ describe('ETCD Registry', () => {
     const etcd = new Etcd(ETCD_SERVER)
 
     etcd.get('services', (error, services) => {
-      if (services) {
+      if (!error && services) {
         etcd.del('services', { recursive: true }, () => {
           etcd.del('services-options', { recursive: true }, done)
         })
@@ -34,9 +34,15 @@ describe('ETCD Registry', () => {
 
       const etcd2 = new ETCDRegistry(ETCD_SERVER)
 
-      expect(etcd2.getService('test').length).to.be.equal(1)
-      expect(etcd2.getService('test1').length).to.be.equal(1)
-      expect(etcd2.getService('test2').length).to.be.equal(1)
+      return Promise.all([
+        etcd2.getService('test'),
+        etcd2.getService('test1'),
+        etcd2.getService('test2')
+      ]).then((results) => {
+        expect(results[ 0 ].length).to.be.equal(1)
+        expect(results[ 1 ].length).to.be.equal(1)
+        expect(results[ 2 ].length).to.be.equal(1)
+      })
     })
   })
 
@@ -46,104 +52,112 @@ describe('ETCD Registry', () => {
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
-      const nodes = etcd.getService('test')
+      return etcd.getService('test')
+        .then((nodes) => {
+          expect(nodes.length).to.be.equal(1)
 
-      expect(nodes.length).to.be.equal(1)
+          const node = nodes[ 0 ]
 
-      const node = nodes[ 0 ]
-
-      expect(node.name).to.be.equal('test')
-      expect(node.connection.address).to.be.equal('test')
+          expect(node.name).to.be.equal('test')
+          expect(node.connection.address).to.be.equal('test')
+        })
     })
 
-    it('should register service and synchronize between all clients', () => {
+    it('should register service and synchronize between all clients', (done) => {
       const etcd = new ETCDRegistry(ETCD_SERVER)
       const etcd2 = new ETCDRegistry(ETCD_SERVER)
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
-      const nodes = etcd2.getService('test')
+      setTimeout(() => {
+        etcd2.getService('test')
+          .then((nodes) => {
+            expect(nodes.length).to.be.equal(1)
 
-      expect(nodes.length).to.be.equal(1)
+            const node = nodes[ 0 ]
 
-      const node = nodes[ 0 ]
+            expect(node.name).to.be.equal('test')
+            expect(node.connection.address).to.be.equal('test')
 
-      expect(node.name).to.be.equal('test')
-      expect(node.connection.address).to.be.equal('test')
+            done()
+          })
+      }, 50)
     })
 
     it('should register service with TTL', (done) => {
-      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 5 })
+      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 1 })
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
-      expect(etcd.getService('test').length).to.be.equal(1)
+      etcd.getService('test')
+        .then((nodes) => {
+          expect(nodes.length).to.be.equal(1)
 
-      setTimeout(() => {
-        expect(etcd.getService('test')).to.be.undefined
-        done()
-      }, 6 * 1000)
-    }).timeout(30 * 1000)
+          setTimeout(() => {
+            etcd.getService('test')
+              .catch((error) => {
+                expect(error.message).to.be.equal('Not found service!')
+                done()
+              })
+          }, 2 * 1000)
+        })
+    }).timeout(3 * 1000)
   })
 
   describe('getServiceOptions()', () => {
-    it('should return options of service', (done) => {
+    it('should return options of service', () => {
       const etcd = new ETCDRegistry(ETCD_SERVER)
 
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
-      etcd.getServiceOptions('test')
+      return etcd.getServiceOptions('test')
         .then((options) => {
           expect(options.a).to.be.equal(1)
           expect(options.b).to.be.equal(2)
-
-          done()
         })
-    }).timeout(30 * 1000)
+    })
 
-    it('should return undefined if servicem does not exist', (done) => {
+    it('should return error if service does not exist', () => {
       const etcd = new ETCDRegistry(ETCD_SERVER)
 
       etcd.register('test123', { address: 'test' }, { a: 1, b: 2 })
 
-      etcd.getServiceOptions('test')
-        .then((options) => {
-          expect(options).to.be.undefined
-
-          done()
+      return etcd.getServiceOptions('test')
+        .catch((error) => {
+          expect(error).to.be.instanceOf(Error)
         })
     }).timeout(30 * 1000)
   })
 
   describe('renew()', () => {
     it('should renew TTL', (done) => {
-      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 5 })
+      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 1 })
 
       const id = etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
-      expect(etcd.getService('test').length).to.be.equal(1)
+      etcd.getService('test')
+        .then((nodes) => {
+          expect(nodes.length).to.be.equal(1)
 
-      setTimeout(() => {
-        etcd.renew('test', id)
-      }, 4 * 1000)
+          setTimeout(() => {
+            etcd.renew('test', id)
+          }, 500)
 
-      setTimeout(() => {
-        expect(etcd.getService('test').length).to.be.equal(1)
-        done()
-      }, 6 * 1000)
-    }).timeout(30 * 1000)
-
-    it('should do nothing if nodes do not exist', () => {
-      const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 5 })
-
-      etcd.renew('test', 9999999)
-    })
+          setTimeout(() => {
+            etcd.getService('test')
+              .then((nodes) => {
+                expect(nodes.length).to.be.equal(1)
+                done()
+              })
+          }, 1200)
+        })
+    }).timeout(3 * 1000)
 
     it('should do nothing if node does not exist', () => {
       const etcd = new ETCDRegistry(ETCD_SERVER, { ttl: 5 })
       etcd.register('test', { address: 'test' }, { a: 1, b: 2 })
 
-      etcd.renew('test', 9999999)
+      return etcd.renew('test', 9999999)
     })
   })
 })
